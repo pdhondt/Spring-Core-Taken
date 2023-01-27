0598de27ff8de6c0af83f9c54663cac90c327a34
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-27 15:08:34 +0100

    Taak 1.6 - Dependency Injection

--- src/main/java/be/vdab/school/console/MyRunner.java
+++ src/main/java/be/vdab/school/console/MyRunner.java
@@ -1,12 +1,25 @@
 package be.vdab.school.console;
 
+import be.vdab.school.exceptions.RepositoryException;
+import be.vdab.school.repositories.LeerlingRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements CommandLineRunner {
+    private final LeerlingRepository leerlingRepository;
+
+    public MyRunner(LeerlingRepository leerlingRepository) {
+        this.leerlingRepository = leerlingRepository;
+    }
+
     @Override
     public void run(String... args) {
-        System.out.println("Welkom in de school");
+        try {
+            leerlingRepository.findAll().forEach(leerling ->
+                    System.out.println(leerling.getNummer() + ":" + leerling.getVoornaam() + " " + leerling.getFamilienaam()));
+        } catch (RepositoryException ex) {
+            ex.printStackTrace(System.err);
+        }
     }
 }
